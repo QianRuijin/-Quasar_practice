c97f541d1d7d27f04389c50ead3c033a6f51735d
QianRuijin <mrqianrj@163.com>
2021-02-24 19:46:03 +0800

    生成二维码测试

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -3,10 +3,74 @@
     <div class="row">
       <div class="content-wrap">
         <item-title :title="'新闻动态'"></item-title>
-        <p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"><span style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, &quot;Microsoft YaHei&quot;;">为宣传中国医疗器械监督管理法规政策，加强国际医疗器械监管法规和技术规范的交流，促进各国医疗器械评审部门之间的经验交流和相互学习借鉴，提高审评的科学性和技术水平，提高企业的遵纪守法意识，促进企业与政府以及企业间的交流，推进新技术标准和技术成果的应用，以进一步提高医疗器械的安全性和有效性，中国食品药品国际交流中心将于2020年11月举办第十一届“中国医疗器械监督管理国际会议”。</span></p><p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"><span style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, &quot;Microsoft YaHei&quot;;">本届会议主题为“完善，创新，为全球监管贡献中国智慧”。会议将有来自国内外医疗器械监管部门，医疗器械技术审查机构，有关医疗器械法规协调的国际组织以及国内外标准化机构、医疗器械检测机构、医疗器械应用技术专家和医疗器械企业的代表进行演讲交流，并在全球抗疫的大环境下，探讨医疗器械领域应对疫情挑战的措施和经验。</span></p><p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"><span style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, &quot;Microsoft YaHei&quot;;">由于新冠疫情的影响，会议对原计划的时间和地点（2020年9月8-11日湖北省武汉市）进行了调整。经与有关部门沟通，拟将会议推延至2020年11月17-20日，在福建省福州市福州海峡国际会展中心举办。为了保障广大参会代表和行业同仁的身体健康和会议效果，我们仍将根据国务院联防联控机制印发的《关于进一步做好重点场所重点单位重点人群新冠肺炎疫情防控相关工作的通知》要求，密切关注疫情发展变化，严格按照国家相关防疫要求举办本次会议。会议仍将采用线下的形式，必要时结合线上及直播等方式，以保证会议参与效果。</span></p><p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"><span style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, &quot;Microsoft YaHei&quot;;">我们热忱地期盼您参加！</span></p><p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 24px; font-family: inherit; white-space: normal; text-align: right;"><span style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 等线;">&nbsp;</span></p><p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: right;"><span style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, &quot;Microsoft YaHei&quot;;">中国食品药品国际交流中心</span></p><p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: right;"><span style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, &quot;Microsoft YaHei&quot;;">2020年7月</span></p><p style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px; padding: 0px; border: 0px; font: inherit;"><br style="box-sizing: border-box; margin: 0px; font-family: &quot;Source Sans Pro&quot;, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif; font-size: 14px; letter-spacing: 1.5px; text-align: justify; white-space: normal; word-spacing: 1px; background-color: rgb(255, 255, 255);"/></p><p><br/></p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"
+        >
+          <span
+            style='box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, "Microsoft YaHei";'
+            >为宣传中国医疗器械监督管理法规政策，加强国际医疗器械监管法规和技术规范的交流，促进各国医疗器械评审部门之间的经验交流和相互学习借鉴，提高审评的科学性和技术水平，提高企业的遵纪守法意识，促进企业与政府以及企业间的交流，推进新技术标准和技术成果的应用，以进一步提高医疗器械的安全性和有效性，中国食品药品国际交流中心将于2020年11月举办第十一届“中国医疗器械监督管理国际会议”。</span
+          >
+        </p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"
+        >
+          <span
+            style='box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, "Microsoft YaHei";'
+            >本届会议主题为“完善，创新，为全球监管贡献中国智慧”。会议将有来自国内外医疗器械监管部门，医疗器械技术审查机构，有关医疗器械法规协调的国际组织以及国内外标准化机构、医疗器械检测机构、医疗器械应用技术专家和医疗器械企业的代表进行演讲交流，并在全球抗疫的大环境下，探讨医疗器械领域应对疫情挑战的措施和经验。</span
+          >
+        </p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"
+        >
+          <span
+            style='box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, "Microsoft YaHei";'
+            >由于新冠疫情的影响，会议对原计划的时间和地点（2020年9月8-11日湖北省武汉市）进行了调整。经与有关部门沟通，拟将会议推延至2020年11月17-20日，在福建省福州市福州海峡国际会展中心举办。为了保障广大参会代表和行业同仁的身体健康和会议效果，我们仍将根据国务院联防联控机制印发的《关于进一步做好重点场所重点单位重点人群新冠肺炎疫情防控相关工作的通知》要求，密切关注疫情发展变化，严格按照国家相关防疫要求举办本次会议。会议仍将采用线下的形式，必要时结合线上及直播等方式，以保证会议参与效果。</span
+          >
+        </p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: justify; text-indent: 2em;"
+        >
+          <span
+            style='box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, "Microsoft YaHei";'
+            >我们热忱地期盼您参加！</span
+          >
+        </p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 24px; font-family: inherit; white-space: normal; text-align: right;"
+        >
+          <span
+            style="box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 等线;"
+            >&nbsp;</span
+          >
+        </p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: right;"
+        >
+          <span
+            style='box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, "Microsoft YaHei";'
+            >中国食品药品国际交流中心</span
+          >
+        </p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 15px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: 2em; font-family: inherit; white-space: normal; text-align: right;"
+        >
+          <span
+            style='box-sizing: border-box; margin: 0px; padding: 0px; border: 0px; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: 14px; line-height: inherit; font-family: 微软雅黑, "Microsoft YaHei";'
+            >2020年7月</span
+          >
+        </p>
+        <p
+          style="box-sizing: border-box; margin-top: 0px; margin-bottom: 0px; padding: 0px; border: 0px; font: inherit;"
+        >
+          <br
+            style='box-sizing: border-box; margin: 0px; font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; font-size: 14px; letter-spacing: 1.5px; text-align: justify; white-space: normal; word-spacing: 1px; background-color: rgb(255, 255, 255);'
+          />
+        </p>
+        <p><br /></p>
+        <div id="qrCode" ref="qrCodeDiv"></div>
       </div>
       <div class="info-wrap column">
-        <div class="info-title">第十一届<br/>中国医疗器械监督管理国际会议</div>
+        <div class="info-title">第十一届<br />中国医疗器械监督管理国际会议</div>
         <div class="info">
           <div class="row justify-between">
             <div class="info-item">会议日期</div>
@@ -31,12 +95,15 @@
 </template>
 
 <script>
+import QRCode from 'qrcodejs2'
 import ItemTitle from '../components/Title'
 export default {
   name: 'Detail',
   data () {
     return {
-      data: {}
+      data: {
+        test: null
+      }
     }
   },
   components: {
@@ -44,11 +111,24 @@ export default {
   },
   mounted () {
     // this.loadData()
+    document.getElementById('qrCode').innerHTML = ''
+    setTimeout(() => {
+      this.test = new QRCode(this.$refs.qrCodeDiv, {
+        text:
+          'http://www.quasarchs.com/vue-components/virtual-scroll/',
+        width: 200,
+        height: 200,
+        colorDark: '#333333', // 二维码颜色
+        colorLight: '#ffffff', // 二维码背景色
+        correctLevel: QRCode.CorrectLevel.L // 容错率，L/M/H
+      })
+    }, 100)
   },
   methods: {
     loadData () {
-      this.$axios.post('/api/con/info/get', { info_id: '1229' })
-        .then((response) => {
+      this.$axios
+        .post('/api/con/info/get', { info_id: '1229' })
+        .then(response => {
           this.data = response.data
           console.log(this.data)
         })
